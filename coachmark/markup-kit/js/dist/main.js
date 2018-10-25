772d88befaa020c1ccc1e1818ff20d6c5dd9ec81
Elias Mason <eliasjmason@gmail.com>
2018-10-25 11:57:12 -0700

    Create function stubs
    	modified:   js/dist/main.js

--- coachmark/markup-kit/js/dist/main.js
+++ coachmark/markup-kit/js/dist/main.js
@@ -2,4 +2,27 @@
 
 (function (w, doc) {
   'use strict';
+
+  var coachmarkTemplate = doc.querySelector('#coachmark'),
+      triggers = doc.querySelectorAll('button[data-type]');
+
+  function renderCoachmark(template, type) {
+    var clone = template.content.cloneNode(true),
+        wrapper = clone.querySelector('#coachmark'),
+        dismissBtn = clone.querySelector('#dismiss');
+
+    doc.body.appendChild(clone);
+  }
+
+  function handleTriggerClick(e) {
+    var type = e.target.dataset.type;
+
+    renderCoachmark(coachmarkTemplate, type);
+  }
+
+  doc.addEventListener('DOMContentLoaded', function () {
+    triggers.forEach(function (trigger) {
+      trigger.addEventListener('click', handleTriggerClick);
+    });
+  });
 })(window, document);
--- coachmark/markup-kit/js/main.js
+++ coachmark/markup-kit/js/main.js
@@ -1,4 +1,25 @@
 (function(w, doc) {
   'use strict';
 
+  const coachmarkTemplate = doc.querySelector('#coachmark'),
+    triggers = doc.querySelectorAll('button[data-type]');
+
+  function renderCoachmark(template, type) {
+    const clone = template.content.cloneNode(true),
+      wrapper = clone.querySelector('#coachmark'),
+      dismissBtn = clone.querySelector('#dismiss');
+    
+    doc.body.appendChild(clone);
+  }
+
+  function handleTriggerClick(e) {
+    const { type } = e.target.dataset;
+    renderCoachmark(coachmarkTemplate, type);
+  }
+
+  doc.addEventListener('DOMContentLoaded', function () {
+    triggers.forEach(function(trigger) {
+      trigger.addEventListener('click', handleTriggerClick);
+    });
+  });
 })(window, document);
