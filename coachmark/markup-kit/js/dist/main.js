2e251edcf4e17521fa151456c73c7ddc789ae893
Elias Mason <eliasjmason@gmail.com>
2018-10-25 12:19:25 -0700

    Finish render function

--- coachmark/markup-kit/js/dist/main.js
+++ coachmark/markup-kit/js/dist/main.js
@@ -3,13 +3,31 @@
 (function (w, doc) {
   'use strict';
 
-  var coachmarkTemplate = doc.querySelector('#coachmark'),
+  var coachmarkTemplate = doc.querySelector('#template'),
+      svg = doc.querySelector('#remove-sm-18'),
       triggers = doc.querySelectorAll('button[data-type]');
 
-  function renderCoachmark(template, type) {
+  function renderCoachmark(template, dismissType) {
     var clone = template.content.cloneNode(true),
-        wrapper = clone.querySelector('#coachmark'),
+        wrapper = clone.firstElementChild,
         dismissBtn = clone.querySelector('#dismiss');
+
+    console.log(wrapper);
+
+    wrapper.setAttribute('data-dismiss-type', dismissType);
+
+    if (dismissType === 'link') {
+      dismissBtn.classList.add('dismiss-link');
+      dismissBtn.textContent = 'Dismiss';
+    }
+
+    if (dismissType === 'button') {
+      var svgCopy = svg.cloneNode(true);
+      svgCopy.removeAttribute('style');
+
+      dismissBtn.classList.add('pe-icon--btn');
+      dismissBtn.appendChild(svgCopy);
+    }
 
     doc.body.appendChild(clone);
   }
--- coachmark/markup-kit/js/main.js
+++ coachmark/markup-kit/js/main.js
@@ -1,14 +1,32 @@
 (function(w, doc) {
   'use strict';
 
-  const coachmarkTemplate = doc.querySelector('#coachmark'),
+  const coachmarkTemplate = doc.querySelector('#template'),
+    svg = doc.querySelector('#remove-sm-18'),
     triggers = doc.querySelectorAll('button[data-type]');
 
-  function renderCoachmark(template, type) {
+  function renderCoachmark(template, dismissType) {
     const clone = template.content.cloneNode(true),
-      wrapper = clone.querySelector('#coachmark'),
+      wrapper = clone.firstElementChild,
       dismissBtn = clone.querySelector('#dismiss');
-    
+
+      console.log(wrapper)
+
+    wrapper.setAttribute('data-dismiss-type', dismissType);
+
+    if (dismissType === 'link') {
+      dismissBtn.classList.add('dismiss-link');
+      dismissBtn.textContent = 'Dismiss';
+    }
+
+    if (dismissType === 'button') {
+      let svgCopy = svg.cloneNode(true);
+      svgCopy.removeAttribute('style');
+
+      dismissBtn.classList.add('pe-icon--btn');
+      dismissBtn.appendChild(svgCopy);
+    }
+
     doc.body.appendChild(clone);
   }
 
@@ -17,7 +35,7 @@
     renderCoachmark(coachmarkTemplate, type);
   }
 
-  doc.addEventListener('DOMContentLoaded', function () {
+  doc.addEventListener('DOMContentLoaded', function() {
     triggers.forEach(function(trigger) {
       trigger.addEventListener('click', handleTriggerClick);
     });
